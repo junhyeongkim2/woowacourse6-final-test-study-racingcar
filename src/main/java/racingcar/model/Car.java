b5de2cebf01524912c689bf16805140f13dcf66a
jun <ggprgrkjh@naver.com>
2023-12-01 14:24:22 +0900

    feat(car): 4 이상일 경우 자동차를 전진시킨다 기능 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+public class Car {
+
+    private final String name;
+    private int location;
+
+    public Car(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public int moveForward(int randomNumber) {
+        if (randomNumber >= 4) {
+            return location += 1;
+        }
+        return location;
+    }
+}
