d11191da6cc8759067613e6dee19202c4f055c5a
jun <ggprgrkjh@naver.com>
2023-12-01 17:28:09 +0900

    refactor(cars): 매직넘버 제거

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -7,6 +7,14 @@ import net.bytebuddy.pool.TypePool.Resolution.Illegal;
 
 public class Cars {
 
+    private static final String COMMA = ",";
+    private static final int LOCATION = 0;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String DUPLICATE_NAME_ERROR = "[ERROR] 중복된 이름이 발생했습니다.";
+    private static final String OVER_FIVE_LENGTH_NAME_ERROR = "[ERROR] 이름은 5자 이하만 가능합니다.";
+    private static final String SPACE_NAME_ERROR = "[ERROR] 이름은 한 글자 이상부터 가능합니다.";
+    private static final String SPACE = "";
+
     private final List<Vehicle> cars;
 
     private Cars(List<Vehicle> cars) {
@@ -14,11 +22,11 @@ public class Cars {
     }
 
     public static Cars of(String carNames) {
-        List<String> names = List.of(carNames.split(","));
+        List<String> names = List.of(carNames.split(COMMA));
         validateDuplicate(names);
         validateIsOverFiveNameLength(names);
         validateNullName(names);
-        return new Cars(names.stream().map(name -> new Car(name, 0)).collect(Collectors.toList()));
+        return new Cars(names.stream().map(name -> new Car(name, LOCATION)).collect(Collectors.toList()));
     }
 
     public List<Vehicle> moveForwardAll() {
@@ -32,22 +40,22 @@ public class Cars {
 
     private static void validateDuplicate(List<String> names) {
         if (names.stream().distinct().count() != names.size()) {
-            throw new IllegalArgumentException("[ERROR] 중복된 이름이 발생했습니다.");
+            throw new IllegalArgumentException(DUPLICATE_NAME_ERROR);
         }
     }
 
     private static void validateIsOverFiveNameLength(List<String> names) {
         for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("[ERROR] 이름은 5자 이하만 가능합니다.");
+            if (name.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException(OVER_FIVE_LENGTH_NAME_ERROR);
             }
         }
     }
 
     private static void validateNullName(List<String> names) {
         for (String name : names) {
-            if (name.equals("")) {
-                throw new IllegalArgumentException("[ERROR] 이름은 한 글자 이상부터 가능합니다.");
+            if (name.equals(SPACE)) {
+                throw new IllegalArgumentException(SPACE_NAME_ERROR);
             }
         }
     }
--- src/main/java/racingcar/model/RandomNumberGenerator.java
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -3,11 +3,15 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+
+    private final static int START_NUMBER = 0;
+    private final static int END_NUMBER = 9;
+
     private RandomNumberGenerator() {
     }
 
     public static int generate() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 
 }
