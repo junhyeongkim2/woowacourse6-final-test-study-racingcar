3fa9c94b4fb89f02d577dc8e242a719c7c571bc1
jun <ggprgrkjh@naver.com>
2023-12-01 15:23:24 +0900

    feat(cars): 자동차 이름을 쉼표 기준으로 구분하고 정적 팩토리 메서드를 통해 cars 생성 기능 추가

--- src/main/java/racingcar/controller/CarRacingController.java
+++ src/main/java/racingcar/controller/CarRacingController.java
@@ -1,4 +1,25 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.view.Input;
+import racingcar.view.InputView;
+import racingcar.view.Output;
+import racingcar.view.OutputView;
+
 public class CarRacingController {
+    Input inputView;
+    Output outputView;
+
+    public CarRacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        Cars cars = Cars.of(inputView.readCarNames());
+
+    }
+
+
 }
--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,13 +1,19 @@
 package racingcar.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
     private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
+    private Cars(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public static Cars of(String carNames) {
+        List<String> names = List.of(carNames.split(","));
+        return new Cars(names.stream().map(name -> new Car(name, 0)).collect(Collectors.toList()));
     }
 
     public List<Car> moveForwardAll() {
