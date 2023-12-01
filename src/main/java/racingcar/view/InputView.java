1826c75c02aeafd78297cb2acbc6934acae0ec29
jun <ggprgrkjh@naver.com>
2023-12-01 16:36:08 +0900

    feat(inputview): 시도 회수에 숫자가 아닌 값이 들어갈 경우 에러 발생 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -15,7 +15,16 @@ public class InputView implements Input {
     public String readTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         String input = Console.readLine();
+        validateReadTryNumber(input);
         return input;
+    }
+
+    private void validateReadTryNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 시도 회수는 숫자만 입력할 수 있습니다.");
+        }
     }
 
 
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -11,5 +11,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
 
+    @AfterEach
+    void setUp() {
+        Console.close();
+    }
+
+    @DisplayName("시도 회수 입력 시 숫자가 아닌 경우 에러 발생 테스트")
+    @Test
+    void readCarNames_IsNotValidForm_ExceptionThrow() {
+        //given
+        ByteArrayInputStream fakeInput = new ByteArrayInputStream("abc".getBytes());
+        System.setIn(fakeInput);
+        InputView inputView = new InputView();
+        //when
+
+        //then
+        assertThatThrownBy(() -> inputView.readTryNumber()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 회수는 숫자만 입력할 수 있습니다.");
+
+    }
 
 }
