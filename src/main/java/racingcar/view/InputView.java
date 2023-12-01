bf4df510a0d0236d09c950596875f34f39117184
jun <ggprgrkjh@naver.com>
2023-12-01 17:32:47 +0900

    refactor(inputview): 매직넘버 제거 및 메세지 상수 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,16 +5,22 @@ import java.util.regex.Pattern;
 
 public class InputView implements Input {
 
+    private final String READ_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private final String READ_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String READ_TRY_NUMBER_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 시도 회수는 숫자만 입력할 수 있습니다.";
+    private final String READ_CAR_NAMES_INVALID_FORM_ERROR_MESSAGE = "[ERROR] 자동차 이름이 형식에 맞지 않습니다.";
+    private final String READ_CAR_NAMES_REGREX_FORM = "^(?:[a-zA-Z가-힣1-9]+,)*[a-zA-Z가-힣1-9]+$";
+
 
     public String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(READ_CAR_NAMES_MESSAGE);
         String input = Console.readLine();
         validateReadCarNamesForm(input);
         return input;
     }
 
     public String readTryNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(READ_TRY_NUMBER_MESSAGE);
         String input = Console.readLine();
         validateReadTryNumber(input);
         return input;
@@ -24,13 +30,13 @@ public class InputView implements Input {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 시도 회수는 숫자만 입력할 수 있습니다.");
+            throw new IllegalArgumentException(READ_TRY_NUMBER_NOT_INTEGER_ERROR_MESSAGE);
         }
     }
 
     private void validateReadCarNamesForm(String input) {
-        if (!Pattern.matches("^(?:[a-zA-Z가-힣1-9]+,)*[a-zA-Z가-힣1-9]+$", input)) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름이 형식에 맞지 않습니다.");
+        if (!Pattern.matches(READ_CAR_NAMES_REGREX_FORM, input)) {
+            throw new IllegalArgumentException(READ_CAR_NAMES_INVALID_FORM_ERROR_MESSAGE);
         }
     }
 
